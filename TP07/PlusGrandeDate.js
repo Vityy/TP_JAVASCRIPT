03f36daf559c25f1db11e931fe1662941eaa9491
Vityy <ygensoftvd@gmail.com>
2025-10-16 16:30:15 +0200

    PlusGrandeDate fait

---
+++ TP07/PlusGrandeDate.js
@@ -0,0 +1,11 @@
+const date1 = new Date("September 19, 1965 09:05:00");
+const date2 = new Date("April 22, 1994 09:15:00");
+
+if(date1 > date2)
+{
+    console.log(date1);
+}
+else
+{
+    console.log(date2);
+}
